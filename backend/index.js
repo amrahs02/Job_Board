69eaff47750229af5fafe876a6e001d0469426db
Sandeep Sharma <sandeepsharma2183@gmail.com>
2024-10-20 13:24:24 +0530

    Refactor CORS options to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: ['http://localhost:5173', 'https://jobboard-81hq.onrender.com'],
+    credentials: true,
+  };
 app.use(cors(corsOptions));
 
 
