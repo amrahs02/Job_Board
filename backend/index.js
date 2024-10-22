f1f309a535eb0bccc051234b9ab0f49cf7295460
Sandeep Sharma <sandeepsharma2183@gmail.com>
2024-10-22 15:44:29 +0530

    Refactor CORS options to allow single origin and update UI styles in components

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:["http://localhost:3000","https://jobboard-81hq.onrender.com"],
+    origin: 'https://jobboard-81hq.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
